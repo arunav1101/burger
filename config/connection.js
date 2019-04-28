fe936fc87c77ed093e88f1ce48f41e7b90712489
Arunav Gupta <arunav1101@gmail.com>
2019-04-27 22:50:18 -0700

    fix code

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,19 @@
-// Set up MySQL connection.
 var mysql = require("mysql");
-var connection ;
+var connection;
 
-if(process.env.JAWDB_URL){
+if (process.env.JAWDB_URL) {
   connection = mysql.createConnection(process.env.JAWDB_URL)
-}else{
-connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "yourRootPassword",
-  database: "burgers_db"
-});
-
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "yourRootPassword",
+    database: "burgers_db"
+  });
+};
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
